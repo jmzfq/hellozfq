9c1449697a3db9789cf2d9ccdc8b5dd84fd4bd5c
张富全 <goah@163.com>
2017-05-16 13:09:48 +0800

    this ad test02

---
+++ src/test01.java
@@ -0,0 +1,12 @@
+
+public class test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=12;
+		int j=23;
+		int k =i + j;
+		System.out.println("我的结果=" + k);
+	}
+
+}
