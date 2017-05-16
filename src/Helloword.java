4b8b7fb5accfda29a5f6b48071766943b2000a20
张富全 <goah@163.com>
2017-05-16 13:04:25 +0800

    进行了修改。

--- src/Helloword.java
+++ src/Helloword.java
@@ -3,9 +3,9 @@ public class Helloword {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("这是我的测试！");
-		// 测试变动情况
-		System.out.print("QQ 多好!")
+		System.out.println("测试一下成功机会");
+		// 娴嬭瘯鍙樺姩鎯呭喌
+		//System.out.print("QQ 澶氬ソ!")
 	}
 
 }
