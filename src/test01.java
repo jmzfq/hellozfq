cde5f21f9ac592c07d31a2c58fff340502caaab8
jmzfq <399808876@qq.com>
2017-05-18 16:23:42 +0800

    Update test01.java

--- src/test01.java
+++ src/test01.java
@@ -7,7 +7,7 @@ public class test01 {
 		int j=23;
 		int k =i + j;
 		System.out.println("结果如下：" + k);//计算会出现有这些问题
-		System.out.println("别难为自己了");
+		System.out.println("别难为自己了");//上传可以了，但是下载行吗？
 		
 	}
 
